3d6fe093e552ca4ebad7f055ac181a1324ad9ecd
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-12 12:19:30 +0100

    Completed Chpt3

---
+++ CsvPaser/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+using CsvParser;
+
+var reader = new StreamReader(new FileStream("Marvel.csv", FileMode.Open));
+
+var csvReader = new CsvReader(reader);
+
+foreach (var line in csvReader.Lines)
+
+Console.WriteLine(line.First(p => p.Key == "Title").Value);
