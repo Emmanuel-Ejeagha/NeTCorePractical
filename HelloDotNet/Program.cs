1e9b78d59b692ad38e97f3c938798dc09fd313f2
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-14 20:50:42 +0100

    Added Figgle

---
+++ HelloDotNet/Program.cs
@@ -0,0 +1,14 @@
+﻿// This project is using global using in .csproj file
+
+using HelloDotNet;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: HelloDotnet <text>");
+    Environment.Exit(1);
+}
+
+// Console.WriteLine(FiggleFonts.Standard.Render(args[0]));
+AsciiArt.Write(args[0]);
+
+WriteLine("Hello World");
