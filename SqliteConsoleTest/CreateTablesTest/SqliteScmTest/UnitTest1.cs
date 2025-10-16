a8456b4094e3806e8ee5fcbefef65379d5323e15
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-16 02:12:29 +0100

    Added SqlLite configuration

---
+++ SqliteConsoleTest/CreateTablesTest/SqliteScmTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace SqliteScmTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ SqliteConsoleTest/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.Sqlite;
+
+using (var connection = new SqliteConnection("Data Source=:memory:"))
+{
+    connection.Open();
+
+    var command = new SqliteCommand(
+        "SELECT 1;", connection);
+    long result = (long)command.ExecuteScalar();
+    Console.WriteLine($"Command output: {result}");
+}
