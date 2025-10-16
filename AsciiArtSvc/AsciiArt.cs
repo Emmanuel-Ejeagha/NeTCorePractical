87272d7455e716aa073e4f48ab814eb031a91df2
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-16 02:14:18 +0100

    Added: Lazy Enumerable

---
+++ AsciiArtSvc/AsciiArt.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Reflection;
+using Figgle;
+
+namespace AsciiArtSvc;
+
+public static class AsciiArt
+{
+    public static string Write(string text, string? fontName = null)
+    {
+        FiggleFont? font = null;
+
+        if (!string.IsNullOrWhiteSpace(fontName))
+        {
+            font = typeof(FiggleFonts).GetProperty(fontName,
+            BindingFlags.Static | BindingFlags.Public)
+            ?.GetValue(null)
+            as FiggleFont;
+        }
+        font ??= FiggleFonts.Standard;
+        return font.Render(text);
+    }
+    public static Lazy<IEnumerable<(string Name, FiggleFont Font)>> AllFonts =
+        new(() =>
+            from p in typeof(FiggleFonts)
+            .GetProperties(
+                BindingFlags.Public | BindingFlags.Static)
+            select
+                (Name: p.Name,
+                Font: (p.GetValue(null) as FiggleFont)
+            ));
+}
