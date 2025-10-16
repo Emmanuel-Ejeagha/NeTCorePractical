628b2f18cfcecd677636ec37db0ab072022f26d0
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-16 02:10:33 +0100

    Csv reader

---
+++ CsvReader/CsvUnitTest/CsvReaderTest.cs
@@ -0,0 +1,28 @@
+﻿using CsvParser;
+
+
+
+namespace CSVUnitTest;
+
+public class CsvReaderTest : IDisposable
+{
+    private StreamReader streamReader;
+    private CsvReader csvReader;
+
+    public CsvReaderTest()
+    {
+        streamReader = new StreamReader(new FileStream("Marvel.csv", FileMode.Open));
+        csvReader = new CsvReader(streamReader);
+    }
+
+    public void Dispose()
+    {
+        streamReader.Dispose();
+    }
+
+    [Fact]
+    public void VerifyNumberOfLines()
+    {
+        Assert.Equal(7, csvReader.Lines.Count());
+    }
+}
