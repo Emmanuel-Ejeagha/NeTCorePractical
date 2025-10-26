bfc2b87008be4dc68386b1cc702c503f6046a7af
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-26 16:40:23 +0100

    Added async

--- Chpt4FileAndNetworkIO/XkcdSearch/Program.cs
+++ Chpt4FileAndNetworkIO/XkcdSearch/Program.cs
@@ -1,11 +1,53 @@
 ﻿using XkcdSearch;
 using CommandLine;
-using CommandLine.Text;
+using System.Diagnostics;
 
-var results = Parser.Default.ParseArguments<Options>(args)
-    .WithParsed<Options>(options => {
-        WriteLine(options.Text);
+static async Task<Comic?> GetComicWithTitleAsync(string title)
+{
+    var cancellationToken = new CancellationTokenSource();
+    var lastComic = await Comic.GetComicAsync(0, cancellationToken.Token);
+    var tasks = new List<Task>();
+    Comic? foundComic = null;
+    
+    for (int number = lastComic!.Number; number > 0; number--)
+    {
+        var localNumber = number;
+        var getComicTask = Comic.GetComicAsync(localNumber, cancellationToken.Token);
+        var continuationTask = getComicTask.ContinueWith(t =>
+        {
+            try
+            {
+                var comic = t.Result;
+                if (comic != null &&
+                  string.Equals(title, comic!.Title, StringComparison.OrdinalIgnoreCase))
+                {
+                    cancellationToken.Cancel();
+                    foundComic = comic;
+                }
+            }
+            catch (TaskCanceledException) { }
+        });
+        tasks.Add(continuationTask);
+    }
+    await Task.WhenAll(tasks);
+    return foundComic;
+}
+
+var results = await Parser.Default.ParseArguments<Options>(args)
+    .WithParsedAsync<Options>(async options =>  // Make this async
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var comic = await GetComicWithTitleAsync(options.Title!);
+        stopwatch.Stop();
+        WriteLine("Result returned in " +
+        $"{stopwatch.ElapsedMilliseconds} ms");
+        if (comic == null)
+        {
+            WriteLine($"xkcd comic with title \"{options.Title}\" not found");
+        }
+        else
+        {
+            WriteLine($"xkcd \"{options.Title}\" is number {comic.Number} published on {comic.Date}");
+        }
     });
-
-results.WithNotParsed(_ => 
-    WriteLine(HelpText.RenderUsageText(results)));
