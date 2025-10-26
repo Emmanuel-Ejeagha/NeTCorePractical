02dc728b419bd438ff25785a74406f33c06426a5
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-26 16:42:04 +0100

    Added: record

--- Chpt4FileAndNetworkIO/XkcdSearch/Options.cs
+++ Chpt4FileAndNetworkIO/XkcdSearch/Options.cs
@@ -2,8 +2,11 @@ using CommandLine;
 
 namespace XkcdSearch;
 
-public record class Options
+public record Options
 {
-    [Value(0, Required = true, HelpText = "Some text")]
-    public string? Text { get;  init;}
+    [Value(0, Required = true, HelpText = "Title")]
+    public string? Title { get; init; }
+
+    [Option('c', "category", HelpText = "Joke category")]
+    public string Category { get; init; } = "programming";
 }
