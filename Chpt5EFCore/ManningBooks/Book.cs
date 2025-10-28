40ace80b0111f62374a12d237a45b8b422e32428
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-28 14:10:59 +0100

    Relationship

--- Chpt5EFCore/ManningBooks/Book.cs
+++ Chpt5EFCore/ManningBooks/Book.cs
@@ -6,8 +6,18 @@ public class Book
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    
-    
-    
-    
+    public Rating rating { get; } // One-to-one Relationship
+    public List<Rating> Ratings { get; } // one to many
+
+    public Book(string title)
+    {
+        Title = title;
+    }
+}
+
+public class Rating
+{
+    public int Id { get; set; }
+    public int BookId { get; set; }
+    public Book Book { get; set; }
 }
