c806bc81c0c517ee4d6aaa9a7b33c0bd880890e1
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-11-01 21:56:42 +0100

    Added Rating

---
+++ Chpt5EFCore/ManningBooksApi/Rating.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ManningBooksApi;
+
+public class Rating
+{
+     public int Id { get; set; }
+    public int Stars { get; set; } = 5;
+     public string? Comment { get; set; }
+
+}
