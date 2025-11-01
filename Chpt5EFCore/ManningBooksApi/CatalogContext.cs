f8cdf6593a15f8cd4ac67faa44a4d1db1a628e0f
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-11-01 21:55:29 +0100

    Added dbcontext

---
+++ Chpt5EFCore/ManningBooksApi/CatalogContext.cs
@@ -0,0 +1,47 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManningBooksApi;
+
+public class CatalogContext : DbContext
+{
+    public const string ConnectionString = "DataSource=manningbooks;mode=memory;cache=shared";
+    public DbSet<Book> Books { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite(ConnectionString);
+    public static void SeedBooks()
+    {
+        using var dbContext = new CatalogContext();
+        if (!dbContext.Database.EnsureCreated())
+        {
+            return;
+        }
+
+        dbContext.Add(new Book("Grokking Simplicity"));
+        dbContext.Add(new Book("API Design Patterns"));
+        var efBook = new Book("EF Core in Action");
+        efBook.Ratings.Add(new Rating { Comment = "Great!" });
+        efBook.Ratings.Add(new Rating { Stars = 4 });
+        dbContext.Add(efBook);
+        dbContext.SaveChanges();
+    }
+
+    public record BookCreateCommand(string Title, string? Description) { }
+    public record BookUpdateCommand(string Title, string? Description) {}
+    
+    // public static async Task WriteBookToConsoleAsync (string title)
+    // {
+    //     using var dbContext = new CatalogContext();
+    //     var book = await dbContext.Books.Include(b => b.Ratings)
+    //       .FirstOrDefaultAsync(b => b.Title == title);
+    //     if (book == null)
+    //     {
+    //         Console.WriteLine(@$"""{title}"" not found.");
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine(@$"Book ""{book.Title}"" has id {book.Id}");
+    //         book.Ratings.ForEach(r => Console.WriteLine($"\t{r.Stars} stars: {r.Comment ?? "-blank-"}"));
+    //     }
+    // }
+}
+
