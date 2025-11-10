82a74bae5ad9cf44340db33fe3cbba4844cf76d9
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-11-10 23:59:56 +0100

    Add IsValid method

---
+++ Chpt6_Unit-Testing/SudokuSolver/Solver.cs
@@ -0,0 +1,16 @@
+﻿namespace SudokuSolver;
+
+public class Solver
+{
+    private readonly int[,] _board;
+
+    public Solver(int[,] board)
+    {
+        _board = board;
+    }
+
+    public bool Isvalid()
+    {
+        return false;
+    }
+}
