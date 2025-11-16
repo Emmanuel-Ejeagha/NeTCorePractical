1c06b7c731f5eaffbeababf9ab92118fb7f9ed7c
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-11-16 08:35:40 +0100

    Add IBoard interface

--- Chpt6_Unit-Testing/SudokuSolver/Solver.cs
+++ Chpt6_Unit-Testing/SudokuSolver/Solver.cs
@@ -11,6 +11,26 @@ public class Solver
 
     public bool Isvalid()
     {
-        return false;
+        int rows = _board.GetLength(0);
+        int cols = _board.GetLength(1);
+
+        if (rows != cols)
+            return false;
+
+        if (rows < 4)
+            return false;
+
+        int sqrt = (int)Math.Sqrt(rows);
+        if ((sqrt * sqrt) != rows)
+            return false;
+
+        return true;
+    }
+
+    public interface IBoard
+    {
+        int this[int row, int column] { get; set; }
+        int Size { get; }
+        int GridSize { get; }
     }
 }
