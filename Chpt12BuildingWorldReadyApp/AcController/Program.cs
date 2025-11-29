335cc5c3ca515d2c8095f78b23279beab2d35769
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-11-29 14:16:11 +0100

    Add Swagger and Localization

---
+++ Chpt12BuildingWorldReadyApp/AcController/Program.cs
@@ -0,0 +1,50 @@
+
+using Microsoft.OpenApi.Models;
+
+namespace AcController;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        // builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "AcController", Version = "v1" });
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            // app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        var cultures = new[] { "en-US", "es-MX" };
+        var localizationOptions = new RequestLocalizationOptions()
+          .SetDefaultCulture(cultures[0])
+          .AddSupportedCultures(cultures)
+          .AddSupportedUICultures(cultures);        
+        app.UseRequestLocalization(localizationOptions);
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
